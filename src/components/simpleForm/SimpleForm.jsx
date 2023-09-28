e24b905c078aacd5669859e9d2d0d17d4627aa5c
mihad-khadem <mihadkhadem@gmail.com>
2023-09-29 03:33:23 +0600

    Access form data

--- src/components/simpleForm/SimpleForm.jsx
+++ src/components/simpleForm/SimpleForm.jsx
@@ -1,8 +1,20 @@
 const SimpleForm = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log('Form submission Done');
+    };
+
     return (
         <div>
-            <form action="">
-                
+            <form id="subBtn" onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Name"/>
+                <br />
+                <input type="email" name="email" id="" placeholder="Email"/>
+                <br />
+                <input type="submit" value="Submit" />
             </form>
         </div>
     );
