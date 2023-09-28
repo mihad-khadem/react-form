a5f47a9952c13791ac8e8b9fd0559c3be7d00b0a
mihad-khadem <mihadkhadem@gmail.com>
2023-09-29 03:09:24 +0600

    Initializing Project

---
+++ src/components/simpleForm/SimpleForm.jsx
@@ -0,0 +1,11 @@
+const SimpleForm = () => {
+    return (
+        <div>
+            <form action="">
+                
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
